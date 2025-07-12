f8b8230d86d490bd53e15a58202210e806e0e3b1
Abuqataada <99135924+Abuqataada@users.noreply.github.com>
2025-07-12 12:46:56 +0100

    added damascusprject pinging

--- ping.py
+++ ping.py
@@ -2,11 +2,17 @@ import requests
 
 def main():
     url = "https://psstech.com.ng/health"
+    url_1 = "https://damascusprojects.onrender.com/health"
     try:
         response = requests.get(url, timeout=10)
         print("Successfully pinged:", url)
         print("Status Code:", response.status_code)
         print("Response Body:", response.text[:200])  # print first 200 chars
+
+        response1 = requests.get(url_1, timeout=10)
+        print("Successfully pinged:", url_1)
+        print("Status Code:", response1.status_code)
+        print("Response Body:", response1.text[:200])  # print first 200 chars
     except Exception as e:
         print("Error:", e)
 
